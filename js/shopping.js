2a6b71d2afab032ab2b1bd1d0cc3ba26f403f2fd
CrownClown00 <rossca.thompson@gmail.com>
2015-07-15 21:26:58 -0400

    alot of Bug Fizes
    99%

--- js/shopping.js
+++ js/shopping.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
+   var check= '<span class=""> </span>';
+   var X = '<span class="Xclass"></span>'
+    
     $('#buttonpress').click(function () {
-        $('.list').append($('<li>', {
-            text: $('.mdl-textfield__input').val()
+        if( $('.mdl-textfield__input').val().trim().length ==0){
+            
+            alert("Please add something to the List");
+        }
+            
+            
+            else{
+            
+            $('.list').append('<li>'+ $('.mdl-textfield__input').val() + X + '</li>');
+                $('.mdl-textfield__input').val("");
+            
 
-        }));
-        
+                              };
         
     });
     
@@ -18,12 +29,44 @@ $(document).ready(function () {
  $(function() {
 $('ul.list').on( "click", 'li',function () {
         
-        //$('ul.list li').css({color:'red'});
-       // alert('pieeeee');
-        //$('ul.list li').addClass("listyle");
+       
         $(this).toggleClass("listyle");
      //$(this).closest('li').addClass('listyle');
 		//$(this).remove();
-    $('listyle').remove();
+    //$('listyle').remove();
         });
      });
+
+
+
+$(document).keypress(function(e) {
+    if(e.which == 13 ) {
+        
+         // $('.list').append('<li>'+ $('.mdl-textfield__input').val() + X + '</li>');
+                //$('.mdl-textfield__input').val("");
+            
+            $('#buttonpress').click();
+            
+    };
+            
+            
+        
+        
+        
+        
+        
+    
+});
+
+
+
+$(document).on("click", ".Xclass", function(){
+    $(this).closest('li').fadeOut(300);
+});
+
+
+
+$(document).on("click", ".btn", function(){
+    $(".list").empty();
+    
+});
